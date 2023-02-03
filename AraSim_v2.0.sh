d9209941acae4c38c8c96f0e0bd614bb0d0f9691
Dennis H. Calderon <dennis.henry.calderon@gmail.com>
2023-02-03 16:57:47 -0500

    Updated env files

---
+++ AraSim_v2.0.sh
@@ -0,0 +1,13 @@
+# .bashrc
+
+module load gnu/4.8.5
+export CC=`which gcc`
+export CXX=`which g++`
+
+# For running ARASim
+source /fs/ess/PAS1960/BiconeEvolutionOSC/new_root/new_root_setup.sh
+source /cvmfs/ara.opensciencegrid.org/v2.0.0/centos7/setup.sh
+
+module load python/3.6-conda5.2
+
+
