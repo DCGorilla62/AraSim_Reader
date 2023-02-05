af21cd26460ec5f4c66974ff6e60ffc8c8a04e07
Dennis H. Calderon <dennis.henry.calderon@gmail.com>
2023-02-05 09:18:25 -0500

    deleted AraSim_112021.sh for use with AraSim (version compiled 11-2021) generated ROOT files

--- AraSim_112021.sh
+++
@@ -1,7 +0,0 @@
-module load gnu/4.8.5
-export CC=`which gcc`
-export CXX=`which g++`
-
-source /cvmfs/ara.opensciencegrid.org/trunk/centos7/setup.sh 
-
-alias AraSim=/cvmfs/ara.opensciencegrid.org/trunk/centos7/source/AraSim/./AraSim
